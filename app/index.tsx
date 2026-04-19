3bc1d2dcb378598dd255960f673fdaa13035ecd9
Pushkar Shukla <pushkar2315@protonmail.com>
2026-04-19 22:10:10 +0530

    Structure of app is ready with nativewind integrated

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">NayanVaani</Text>
+    </View>
+  );
+}
